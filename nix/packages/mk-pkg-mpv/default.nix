bbbe14e4323ad2aadca90b76cf26a0babdd57294
DMJGilbert <dmjgilbert@me.com>
2025-05-16 18:58:40 +0100

    feat: enable avfoundation on macos

--- nix/packages/mk-pkg-mpv/default.nix
+++ nix/packages/mk-pkg-mpv/default.nix
@@ -212,6 +212,7 @@ in
       MACOS_OPTIONS=(
         `# audio output features`
         -Dcoreaudio=enabled `# CoreAudio audio output`
+        -Davfoundation=enabled
 
         `# video output features`
         -Dcocoa=enabled `# Cocoa` `# BUG: required in audio mode since v0.36.0`
